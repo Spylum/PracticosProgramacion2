843f98950469f36934718e2cd71004394d40a422
juanma-dls <juanmadls6@gmail.com>
2021-05-03 19:08:41 -0300

    commit

--- TrabajoPractico2/app.js
+++ TrabajoPractico2/app.js
@@ -1,7 +1,7 @@
 const Auto = require("./Auto");
 
-const auto1 = ("Chevrolet","Camaro","294",170);
-const auto2 = ("Ford","Mustang","290",173);
-const auto3 = ("Audi","R8","285",178);
-const auto4 = ("BMW","Serie 4","290",175);
-const auto5 = ("Porsche","Carrera","280",176);
+const auto1 = ("Chevrolet","Camaro",294,170);
+const auto2 = ("Ford","Mustang",290,173);
+const auto3 = ("Audi","R8",285,178);
+const auto4 = ("BMW","Serie 4",290,175);
+const auto5 = ("Porsche","Carrera",280,176);
