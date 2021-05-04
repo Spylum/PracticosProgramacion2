bd978ab76e778b4b33c260d00bbaea229372940a
juanma-dls <juanmadls6@gmail.com>
2021-05-04 18:04:33 -0300

    punto 1 sin testear

--- TrabajoPractico2/Auto.js
+++ TrabajoPractico2/Auto.js
@@ -5,6 +5,17 @@ class Auto {
         this.potencia = potencia;
         this.maxVelocidad = maxVelocidad;
     }
+    
+    mostrarAuto() {
+        return `El Auto es un ${this.marca}, ${this.modelo}, tiene una potencia de ${this.potencia} hp y su velocidad max. es de ${this.maxVelocidad} km/h.`
+    }
+
+    
+    calcularTiemp () {
+        let tiempo = 250/this.maxVelocidad;
+        // 250  es la distancia que recorre a su maxima velociadad. 
+        return `El auto ${this.modelo} recorrio 250 km en ${tiempo.toFixed(2)} hs a su velocidad máxima.`
+    }
 }
 
 
--- TrabajoPractico2/app.js
+++ TrabajoPractico2/app.js
@@ -1,7 +1,21 @@
 const Auto = require("./Auto");
 
-const auto1 = ("Chevrolet","Camaro",294,170);
-const auto2 = ("Ford","Mustang",290,173);
-const auto3 = ("Audi","R8",285,178);
-const auto4 = ("BMW","Serie 4",290,175);
-const auto5 = ("Porsche","Carrera",280,176);
+const camaro = new Auto("Chevrolet","Camaro",294,170);
+const mustang = new Auto("Ford","Mustang",290,173);
+const r8 = new Auto("Audi","R8",285,178);
+const serie4 = new Auto("BMW","Serie 4",290,175);
+const carrera = new Auto("Porsche","Carrera",280,176);
+
+console.info(camaro.mostrarAuto());
+console.info(mustang.mostrarAuto());
+console.info(r8.mostrarAuto());
+console.info(serie4.mostrarAuto());
+console.info(carrera.mostrarAuto());
+
+console.info("=============================================================")
+
+console.info(camaro.calcularTiemp());
+console.info(mustang.calcularTiemp());
+console.info(r8.calcularTiemp());
+console.info(serie4.calcularTiemp());
+console.info(carrera.calcularTiemp());
