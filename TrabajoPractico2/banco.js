f49b344703f636c5638414a36aeb9722f838f55a
juanma-dls <juanmadls6@gmail.com>
2021-05-07 18:59:16 -0300

    falta revisar punto 1

--- TrabajoPractico2/banco.js
+++ TrabajoPractico2/banco.js
@@ -1,6 +1,36 @@
 class CuentaBancaria {
-    constructor (fondos) {
-        this.fondos = fondos
-    
+    constructor (fondos, agregar, sacar) {
+        this.fondos = fondos;
+        this.agregar = agregar;
+        this.sacar = sacar;
     }
+
+    asignarFondos () {
+        if (this.agregar >= 5000000) {
+            console.info("No se puede asignar mas de 500000 a los fondos existentes");
+        }
+        return this.fondos + this.agregar;
+    }
+
+    sacarFondos () {
+        if (this.sacar >= 100000) {
+            console.info("No se puede sacar mas de 100000")
+        }
+        else {
+            if (this.sacar > this.fondos) {
+            console.info("No se puede mas dinero que lo que hay en los fondos");
+            }
+            else {
+                return this.fondos - this.sacar;
+            }
+        }
+    }  
 }
+
+const cuenta1 = new CuentaBancaria (10000, 5000, 110000);
+const cuenta2 = new CuentaBancaria (10000, 5000000, 3000 );
+const cuenta3 = new CuentaBancaria (10000, 6000, 5000);
+
+
+console.info(cuenta1.asignarFondos());
+console.info(cuenta1.sacarFondos());
---
+++ TrabajoPractico2/escuela.js
@@ -0,0 +1,13 @@
+class Escuela {
+    constructor(curso, alumno1, alumno2){
+        this.curso = curso;
+        this.alumno1 = alumno1;
+        this.alumno2 = alumno2;
+    }
+}
+
+const curso1 = new Escuela("Desarrollo de software", "Juan", "Manuel");
+const curso2 = new Escuela("Informatica","Jose", "Pablo");
+
+
+
