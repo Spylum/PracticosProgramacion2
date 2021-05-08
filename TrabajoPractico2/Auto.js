35819976d687a75e6accd3887ad51b8ea534ced0
juanma-dls <juanmadls6@gmail.com>
2021-05-07 23:38:20 -0300

    Tp2 terminado

--- TrabajoPractico2/Auto.js
+++ TrabajoPractico2/Auto.js
@@ -7,35 +7,46 @@ class Auto {
     }
     
     mostrarAuto() {
-        return `El Auto es un ${this.marca}, ${this.modelo}, tiene una potencia de ${this.potencia} hp y su velocidad max. es de ${this.maxVelocidad} km/h.`
+        console.info(`El Auto es un ${this.marca}, ${this.modelo}, tiene una potencia de ${this.potencia} hp y su velocidad max. es de ${this.maxVelocidad} km/h.`)
     }
 
-    
     calcularTiemp () {
-        let tiempo = 250/this.maxVelocidad;
+        let tiempo = km /this.maxVelocidad;
         // 250  es la distancia que recorre a su maxima velociadad. 
-        return `El auto ${this.modelo} recorrio 250 km en ${tiempo.toFixed(2)} hs a su velocidad máxima.`
+        console.info(`El auto ${this.modelo} recorrio ${km} km en ${tiempo.toFixed(2)} hs a su velocidad máxima.`)
+        
+        
     }
 }
+let km = 250
 
-const camaro = new Auto("Chevrolet","Camaro",294,170);
-const mustang = new Auto("Ford","Mustang",290,173);
-const r8 = new Auto("Audi","R8",285,178);
-const serie4 = new Auto("BMW","Serie 4",290,175);
-const carrera = new Auto("Porsche","Carrera",280,176);
+const autos = [new Auto("Chevrolet","Camaro",294,170), 
+               new Auto("Ford","Mustang",290,173), 
+               new Auto("Audi","R8",285,178), 
+               new Auto("BMW","Serie 4",290,175), 
+               new Auto("Porsche","Carrera",280,176)];
 
-console.info(camaro.mostrarAuto());
-console.info(mustang.mostrarAuto());
-console.info(r8.mostrarAuto());
-console.info(serie4.mostrarAuto());
-console.info(carrera.mostrarAuto());
+for (let i of autos) {
+        // se muestran los autos por pantalla
+        i.mostrarAuto();
+        
+        if (km == 0) {
+            i.calcularTiemp();
+            console.info("El auto no se ha movido")  
+        }
+        else{
+        // se calcula el tiempo en recorrer en su velocidad maxima
+            i.calcularTiemp();
+        }
 
-console.info("=============================================================")
+    //salto de linea improvisado
+    console.info(" ")
 
-console.info(camaro.calcularTiemp());
-console.info(mustang.calcularTiemp());
-console.info(r8.calcularTiemp());
-console.info(serie4.calcularTiemp());
-console.info(carrera.calcularTiemp());
+}
 
-module.exports = Auto;
+if (typeof km != "number") {
+    console.info("Los kilometros se tiene que ingresar con numeros")
+//salto de linea improvisado
+console.info(" ")
+}
+
--- TrabajoPractico2/banco.js
+++ TrabajoPractico2/banco.js
@@ -7,21 +7,23 @@ class CuentaBancaria {
 
     asignarFondos () {
         if (this.agregar >= 5000000) {
-            console.info("No se puede asignar mas de 500000 a los fondos existentes");
+            console.info("ERROR. No se puede asignar mas de 500000 a los fondos existentes");
         }
-        return this.fondos + this.agregar;
+        else {
+            console.info (this.fondos + this.agregar);
+        }
     }
 
     sacarFondos () {
         if (this.sacar >= 100000) {
-            console.info("No se puede sacar mas de 100000")
+            console.info("ERROR. No se puede sacar mas de 100000");
         }
         else {
             if (this.sacar > this.fondos) {
-            console.info("No se puede mas dinero que lo que hay en los fondos");
+            console.info("ERROR. No se puede sacar mas dinero de lo que hay en los fondos");
             }
             else {
-                return this.fondos - this.sacar;
+                console.info (this.fondos - this.sacar);
             }
         }
     }  
@@ -29,8 +31,18 @@ class CuentaBancaria {
 
 const cuenta1 = new CuentaBancaria (10000, 5000, 110000);
 const cuenta2 = new CuentaBancaria (10000, 5000000, 3000 );
-const cuenta3 = new CuentaBancaria (10000, 6000, 5000);
+const cuenta3 = new CuentaBancaria (10000, 600, 50000);
 
 
-console.info(cuenta1.asignarFondos());
-console.info(cuenta1.sacarFondos());
+cuenta1.asignarFondos();
+cuenta1.sacarFondos();
+
+console.info("=============================================================")
+
+cuenta2.asignarFondos();
+cuenta2.sacarFondos();
+
+console.info("=============================================================")
+
+cuenta3.asignarFondos();
+cuenta3.sacarFondos();
